d68b0737ac73839ed74f477d2ca5549c153bc5ca
Konstantin <vorlog03@gmail.com>
2020-12-11 17:41:20 +0300

    1.0

---
+++ App3Tests/ViewModels/VigCipherTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App3.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App3.ViewModels.Tests
+{
+    [TestClass()]
+    public class VigCipherTests
+    {
+        public string CryptoText = "бщцфаирщри, бл ячъбиуъ щбюэсяёш гфуаа!!!";
+        public string DecryptoText = "поздравляю, ты получил исходный текст!!!";
+        public string Key = "скорпион";
+
+        [TestMethod()]
+        public void EncryptTest()
+        {
+            var ciprner = new VigCipher();
+            string rez = ciprner.Encrypt(DecryptoText, Key);
+            Assert.AreEqual(CryptoText,rez);
+        }
+
+        [TestMethod()]
+        public void DecryptTest()
+        {
+            var ciprner = new VigCipher();
+            string rez = ciprner.Decrypt(CryptoText, Key);
+            Assert.AreEqual(DecryptoText, rez);
+        }
+    }
+}
